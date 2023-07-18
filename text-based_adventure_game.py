7bd0d1597fbdb1b43938961f3dd028e7b2128965
Wade Nelson <nelson.wade.nelson@gmail.com>
2023-07-17 20:40:00 -0400

    oops left a debug print statement

--- text-based_adventure_game.py
+++ text-based_adventure_game.py
@@ -294,14 +294,12 @@ class Game:
     def load_entities(self):
         for root, dirs, files in os.walk("Data"):
             root = os.path.join(root).replace("\\","/")
-            print(root)
             for file in files:
                 if file.endswith(".json"):
                     file_path = os.path.join(root, file)
                     with open(file_path, "r") as data:
                         match root:
                             case "Data/Words":
-                                print(root)
                                 entity = Entity.from_json(data.read())
                                 self.words[entity.name] = entity
                             case _:
